3e657f29fd2e3e1c26e8ede8c02ed639a0f9d3af
hardynoah <90661404+hardynoah@users.noreply.github.com>
2023-12-23 13:35:54 -0500

    did some stuff, box is still broken

--- scenes/box.gd
+++ scenes/box.gd
@@ -1,13 +1,2 @@
-extends CharacterBody2D
+extends RigidBody2D
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-
-
-func _physics_process(delta):
-	# Add the gravity.
-	if not is_on_floor():
-		velocity.y += gravity * delta
-
-
-	move_and_slide()
